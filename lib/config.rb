737401f02b42ac8506b9d0d40c70cc863e821951
Chris Berry <clberry7107@users.noreply.github.com>
2023-12-08 16:17:07 -0500

    Update config.rb
    Deleted API Keys from public view.

--- lib/config.rb
+++ lib/config.rb
@@ -1,11 +1,1 @@
-#! /usr/bin/ruby 
 
-#api keys
-module API_KEYS
-    WEATHER= "c623159a28e345c981be3dde87b6df30"
-    GEOAPIFY = "bd89a2125bb849beb96be2d27f5ea994"
-end
-
-module FILE_NAMES
-    DB = "data/tcdata.db"
-end
